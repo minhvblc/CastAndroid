d509cbbcc41d96c1b9330773f75b2096c68018ac
Minh Nguyễn <ngducminhvblc@gmail.com>
2023-06-13 15:18:07 +0700

    doiing

--- CastAndroid/Package.swift
+++ CastAndroid/Package.swift
@@ -12,8 +12,9 @@ let package = Package(
             targets: ["CastAndroid"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/kewlbear/nodejs-ios.git", branch: "main"),
+        .package(url: "https://github.com/kewlbear/NodeDecoder.git", branch: "main"),
+        .package(url: "https://github.com/kewlbear/NodeBridge.git", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- CastAndroid/Sources/CastAndroid/CastAndroid.swift
+++
@@ -1,6 +0,0 @@
-public struct CastAndroid {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
